4b0d5dd60ff6577565cabdf0cda203db270b6e0d
Min Khant Thu <dsalpha3367@gmail.com>
2024-08-21 21:33:50 +0630

    [Add] Worker BackgroundService implement

--- DotNet8.SignalRNotification.WebApi/Program.cs
+++ DotNet8.SignalRNotification.WebApi/Program.cs
@@ -1,3 +1,4 @@
+using DotNet8.SignalRNotification.WebApi;
 using DotNet8.SignalRNotification.WebApi.Hubs;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -21,6 +22,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.MapHub<StockHub>("/hubs/stock");
+
+builder.Services.AddHostedService<Worker>();
 
 var summaries = new[]
 {
---
+++ DotNet8.SignalRNotification.WebApi/Worker.cs
@@ -0,0 +1,42 @@
+﻿
+using DotNet8.SignalRNotification.WebApi.Hubs;
+using Microsoft.AspNetCore.SignalR;
+
+namespace DotNet8.SignalRNotification.WebApi
+{
+    public class Worker : BackgroundService
+    {
+        private readonly ILogger _logger;
+        private readonly IHubContext<StockHub> _stockHub;
+        private const string stockName = "Basic stock Name";
+        private decimal stockPrice = 100;
+
+        public Worker(ILogger logger, IHubContext<StockHub> stockHub)
+        {
+            _logger = logger;
+            _stockHub = stockHub;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while(!stoppingToken.IsCancellationRequested)
+            {
+                try
+                {
+                    _logger.LogInformation("Worker running at:{time}",DateTimeOffset.Now);
+                    Random rn=new Random();
+                    decimal stockRaise = rn.Next(0, 10000);
+                    string[] stockNames = { "Apple", "Google", "OpenAi", "Microsoft", "Amazon" };
+                    var stockName = stockNames[rn.Next(0,stockNames.Length)];
+                    await _stockHub.Clients.All.SendAsync("Receive StockPrice", stockName, stockPrice);
+                    _logger.LogInformation("Send stock price: {stockName} - {stockRaise}",stockName,stockRaise);
+                    await Task.Delay(4000,stoppingToken);
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex, "Error sending stock price");
+                }
+            }
+        }
+    }
+}
